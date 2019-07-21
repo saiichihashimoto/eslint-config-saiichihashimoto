44db5ac1a21f1eb667a9e770f0be2f8d18c9a84b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-21 16:18:13 -0700

    feat(rules): defining jsx-a11y rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,13 +51,13 @@ module.exports = {
 	},
 	plugins: [
 		'import',
+		'jsx-a11y',
 		'react',
 	],
 
 	/*
 	 *plugins: [
 	 *    'css-modules',
-	 *    'jsx-a11y',
 	 *    'node',
 	 *    'promise',
 	 *    'react-hooks',
@@ -574,6 +574,88 @@ module.exports = {
 			'newlines-between': 'always',
 		}],
 		'import/prefer-default-export': 'off',
+
+		// https://github.com/evcohen/eslint-plugin-jsx-a11y
+		'jsx-a11y/accessible-emoji': 'error',
+		'jsx-a11y/alt-text':         ['error', {
+			'elements':            ['img', 'object', 'area', 'input[type="image"]'],
+			'area':                [],
+			'img':                 [],
+			'input[type="image"]': [],
+			'object':              [],
+		}],
+		'jsx-a11y/anchor-has-content': ['error', { components: ['Link'] }],
+		'jsx-a11y/anchor-is-valid':    ['error', {
+			aspects:     ['noHref', 'invalidHref', 'preferButton'],
+			components:  ['Link'],
+			specialLink: ['to'],
+		}],
+		'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
+		'jsx-a11y/aria-props':                         'error',
+		'jsx-a11y/aria-proptypes':                     'error',
+		'jsx-a11y/aria-role':                          ['error', { ignoreNonDom: false }],
+		'jsx-a11y/aria-unsupported-elements':          'error',
+		'jsx-a11y/click-events-have-key-events':       'warn',
+		'jsx-a11y/heading-has-content':                'error',
+		'jsx-a11y/html-has-lang':                      'error',
+		'jsx-a11y/iframe-has-title':                   'error',
+		'jsx-a11y/img-redundant-alt':                  'warn',
+		'jsx-a11y/interactive-supports-focus':         'error',
+		'jsx-a11y/label-has-associated-control':       ['error', {
+			assert:            'both',
+			controlComponents: [],
+			depth:             25,
+			labelAttributes:   [],
+			labelComponents:   [],
+		}],
+		'jsx-a11y/lang':              'error',
+		'jsx-a11y/media-has-caption': ['error', {
+			audio: [],
+			video: [],
+			track: [],
+		}],
+		'jsx-a11y/mouse-events-have-key-events':                  'error',
+		'jsx-a11y/no-access-key':                                 'error',
+		'jsx-a11y/no-autofocus':                                  'warn',
+		'jsx-a11y/no-distracting-elements':                       ['error', { elements: ['marquee', 'blink'] }],
+		'jsx-a11y/no-interactive-element-to-noninteractive-role': ['error', { tr: ['none', 'presentation'] }],
+		'jsx-a11y/no-noninteractive-element-interactions':        ['error', {
+			handlers: [
+				'onClick',
+				'onMouseDown',
+				'onMouseUp',
+				'onKeyPress',
+				'onKeyDown',
+				'onKeyUp',
+			],
+		}],
+		'jsx-a11y/no-noninteractive-element-to-interactive-role': ['error', {
+			li:    ['menuitem', 'option', 'row', 'tab', 'treeitem'],
+			ol:    ['listbox', 'menu', 'menubar', 'radiogroup', 'tablist', 'tree', 'treegrid'],
+			table: ['grid'],
+			td:    ['gridcell'],
+			ul:    ['listbox', 'menu', 'menubar', 'radiogroup', 'tablist', 'tree', 'treegrid'],
+		}],
+		'jsx-a11y/no-noninteractive-tabindex': ['error', {
+			roles: ['tabpanel'],
+			tags:  [],
+		}],
+		'jsx-a11y/no-onchange':                    'warn',
+		'jsx-a11y/no-redundant-roles':             'error',
+		'jsx-a11y/no-static-element-interactions': ['error', {
+			handlers: [
+				'onClick',
+				'onMouseDown',
+				'onMouseUp',
+				'onKeyPress',
+				'onKeyDown',
+				'onKeyUp',
+			],
+		}],
+		'jsx-a11y/role-has-required-aria-props': 'error',
+		'jsx-a11y/role-supports-aria-props':     'error',
+		'jsx-a11y/scope':                        'error',
+		'jsx-a11y/tabindex-no-positive':         'error',
 
 		// https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
 		'react/boolean-prop-naming':                  'off',
