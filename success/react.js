08dc19a3f5aeaf0268e831d8abc8ecd35c18cb2c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2020-08-08 02:06:05 -0700

    fix(rules): react components are arrow functions

--- success/react.js
+++ success/react.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 
-export default function Component() {
-	return (
-		<div>
-			<span />
-			Hello World
-		</div>
-	);
-}
+const Component = () => (
+	<div>
+		<span />
+		Hello World
+	</div>
+);
+
+export default Component;
