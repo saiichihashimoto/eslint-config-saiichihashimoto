d1c4167089d043ce8a744ca2bacb1b31fe5f24bd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2020-10-02 14:47:50 -0500

    fix(unicorn): import-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1017,6 +1017,7 @@ module.exports = {
 		'template-tag-spacing':                      ['error', 'never'],
 		'unicode-bom':                               ['error', 'never'],
 		'unicorn/better-regex':                      'error',
+		'unicorn/import-style':                      'off',
 		'unicorn/catch-error-name':                  ['error', { name: 'err' }],
 		'unicorn/consistent-function-scoping':       'off',
 		'unicorn/custom-error-definition':           'error',
