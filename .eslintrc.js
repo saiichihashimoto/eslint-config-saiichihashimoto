ef8d90ee61b38a6e109a28fecf71b8aff082d79e
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2020-12-11 13:51:59 -0600

    fix(promise): disabling prefer-await-to-callbacks

--- .eslintrc.js
+++ .eslintrc.js
@@ -757,7 +757,7 @@ module.exports = {
 		'promise/no-return-in-finally':         'error',
 		'promise/no-return-wrap':               'error',
 		'promise/param-names':                  'error',
-		'promise/prefer-await-to-callbacks':    'error',
+		'promise/prefer-await-to-callbacks':    'off',
 		'promise/prefer-await-to-then':         'error',
 		'promise/valid-params':                 'error',
 		'quote-props':                          ['error', 'consistent-as-needed'],
