b0c364ae6fdaf87940db6b21e94192658aec5f78
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2021-06-30 14:57:14 -0500

    fix(lodash): removing you-dont-need-lodash

--- .eslintrc.js
+++ .eslintrc.js
@@ -120,10 +120,9 @@ module.exports = {
 		'react',
 		'react-hooks',
 		'unicorn',
-		'you-dont-need-lodash-underscore',
 		'you-dont-need-momentjs',
 	],
-	extends:  ['plugin:you-dont-need-lodash-underscore/all', 'plugin:you-dont-need-momentjs/recommended'],
+	extends:  ['plugin:you-dont-need-momentjs/recommended'],
 	settings: {
 		'import/extensions': [
 			'.node.mjs',
